c2567773177d803954f6607804d67fd1cfc21fa7
QuL <qvlywk@gmail.com>
2025-03-11 23:18:49 +0800

    update 242 best func

--- src/main/java/com/qvl/solution/hashmap/easy/ValidAnagram_242.java
+++ src/main/java/com/qvl/solution/hashmap/easy/ValidAnagram_242.java
@@ -26,15 +26,19 @@ public class ValidAnagram_242 {
         String t2 = "car";
 
         boolean result1 = isAnagram(s1, t1); // true
-        System.out.printf("output: %s", result1);
-
-        System.out.println();
+        System.out.printf("output: %s%n", result1);
 
         boolean result2 = isAnagram(s2, t2); // false
-        System.out.printf("output: %s", result2);
+        System.out.printf("output: %s%n", result2);
 
+        boolean result3 = isAnagramByAtoZ(s1, t1);
+        System.out.printf("output: %s%n", result3);
+
+        boolean result4 = isAnagramByAtoZ(s2, t2);
+        System.out.printf("output: %s", result4);
     }
 
+    // time complexity O(n) & space complexity O(1)
     public static boolean isAnagram(String s, String t) {
         // 如果s字串和t字串長度不一樣，一定不是字母重排
         if (s.length() != t.length()) {
@@ -60,6 +64,30 @@ public class ValidAnagram_242 {
             int currentCount = countMap.get(alphabet);
             int newCount = currentCount - 1;
             countMap.put(alphabet, newCount);
+        }
+        return true;
+    }
+
+    // time complexity O(n) & space complexity O(1) more fast (best func)
+    public static boolean isAnagramByAtoZ(String s, String t) {
+        // 如果s字串和t字串長度不一樣，一定不是字母重排
+        if (s.length() != t.length()) {
+            return false;
+        }
+        // a-z
+        int[] count = new int[26];
+        // 先計算 字母在s相對應index 出現的次數
+        for (char alphabet : s.toCharArray()) {
+            // ascii 97~126 = a~z
+            // if alphabet = a , a - a = count[0] 先賦值再 + 1
+            count[alphabet - 'a']++;
+        }
+        // 出現在 t 陣列 就 -1，如果 < 0，代表出現次數超過s陣列的次數
+        for (char alphabet : t.toCharArray()) {
+            // if alphabet = c ,--count[2] = 先-1 再賦值 => count[1] 再比較 < 0
+            if (--count[alphabet - 'a'] < 0) {
+                return false;
+            }
         }
         return true;
     }
